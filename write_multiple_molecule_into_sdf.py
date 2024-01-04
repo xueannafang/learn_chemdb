a15c8887ba2bebe4179fc657cabf73f3fb376acd
Xue Fang <sh19129@bristol.ac.uk>
2024-01-04 09:09:36 +0000

    Update write_multiple_molecule_into_sdf.py

--- write_multiple_molecule_into_sdf.py
+++ write_multiple_molecule_into_sdf.py
@@ -14,4 +14,37 @@ sids = res.text.split()
 
 #print(len(sids))
 
-#Step 2: chunk all sids into blocks of 50 each to a
+#Step 2: chunk all sids into blocks of 50 each to abide the using policy, get total number of chunks
+chunk_size = 50
+
+if len(sids) % chunk_size == 0:
+    num_chunks = int(len(sids)/chunk_size)
+else:
+    num_chunks = int(len(sids)/chunk_size) + 1
+
+#Step 3: prepare the sdf to write
+file_name = f"cid2sids_with_cid_{cid}.sdf" #this name means convert cid to sids with cid determined by user at the beginning
+
+with open(file_name, "w") as f:
+    for i in range(num_chunks):
+        print("Processing chunk:", i)
+        idx_init = chunk_size * i #the index of starting sid of this chunk
+        idx_final = chunk_size * (i+1) #the index of final sid of this chunk
+        these_sids = ",".join(sids[idx_init:idx_final]) #note that slicing in this way would not cover idx_final itself
+        url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/" + these_sids + "/record/sdf" #note the substance, sid, record, sdf
+        res = requests.get(url)
+        f.write(res.text) #here we don't need rstrip as we need to keep the \n
+        time.sleep(0.2)
+
+
+#optional: see how it looks like
+with open(file_name, "r") as f:
+    this_file = f.read()
+    print(this_file)
+
+#Next, we can do other things based on the sdf we just obtained, for example, get their smiles one by one, get their structures, etc. Describe in other documents.
+
+
+
+
+
